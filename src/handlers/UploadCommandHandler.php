938e61c033767b5adade15dc8f51c44fb1799132
Parveen Sangroya <9700282+sangroya@users.noreply.github.com>
2022-02-03 15:14:56 +0530

    Update UploadCommandHandler.php
    call to undefined function GuzzleHttp\Psr7\try_fopen() in

--- src/handlers/UploadCommandHandler.php
+++ src/handlers/UploadCommandHandler.php
@@ -42,12 +42,12 @@ final class UploadCommandHandler extends Handler
      *
      * @return StreamInterface
      */
-    protected function sourceToStream($source): StreamInterface
+   protected function sourceToStream($source): StreamInterface
     {
         if (is_string($source)) {
-            $source = Psr7\try_fopen($source, 'r+');
+            $source = Psr7\Utils::tryFopen($source, 'r+');
         }
 
-        return Psr7\stream_for($source);
+        return Psr7\Utils::streamFor($source);
     }
 }
